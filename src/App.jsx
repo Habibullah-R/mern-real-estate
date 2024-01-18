93759e29d13e6ed5ccf9d07c5ca94785e972e19c
Habibullah <habibullahrashid150@gmail.com>
2024-01-18 12:08:56 +0500

    MERN

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import { BrowserRouter , Routes ,Route } from 'react-router-dom'
+import About from './pages/About'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Profile from './pages/Profile'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/profile" element={<Profile/>}/>
+      <Route path="/sign-up" element={<SignUp/>}/>
+      <Route path="/sign-in" element={<SignIn/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
